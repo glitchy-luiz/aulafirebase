81c70f9f8527ec8e5898aa09542594115da42325
Luiz Fernando <84513178+glitchy-luiz@users.noreply.github.com>
2023-09-19 13:33:21 -0300

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+
+  firebaseConfig : {
+  apiKey: "AIzaSyCRhhhNSeu12q5rjCWD74xD62wjvpJGw-E",
+  authDomain: "aulaionic-a91ef.firebaseapp.com",
+  projectId: "aulaionic-a91ef",
+  storageBucket: "aulaionic-a91ef.appspot.com",
+  messagingSenderId: "780381867068",
+  appId: "1:780381867068:web:af78ab2904c193cb4c8739",
+  measurementId: "G-KTR8KLDHY5"
+}
+};
